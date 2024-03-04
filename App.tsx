1e70b274fda551b986e847c71a35c8d6e2717850
Armandopru <ajtorres500@soy.sena.edu.co>
2024-03-04 13:08:20 -0500

    Modificacion apariencia

--- App.tsx
+++ App.tsx
@@ -1,11 +1,22 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image} from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Image
+      source={require('./assets/wall.jpg')}
+      style={styles.imageBackground}
+      />
+      <View style={styles.logoC}>
+        <Image
+        source={require('./assets/Logos.jpg')}
+        style={styles.logoImage}
+        />
+        <Text style={styles.logoT}>S R M Express</Text>
+      </View>
+      <View style={styles.form}
+      ></View>
     </View>
   );
 }
@@ -13,8 +24,40 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: 'black',
   },
+  imageBackground:{
+    width: '100%',
+    height: '100%',
+    opacity: 0.7,
+    bottom: '30%'
+  },
+  form:{ 
+    width: '100%',
+    height: '40%',
+    backgroundColor: 'white',
+    position: 'absolute',
+    bottom: 0,
+    borderTopLeftRadius: 40,
+    borderTopRightRadius: 40,
+  },
+  logoC:{
+    position: 'absolute',
+    alignSelf: 'center',
+    top: '15%',
+  },
+  logoImage:{
+    width: 130,
+    height: 150,
+  },
+  logoT:{
+    color: 'white',
+    textAlign:'center',
+    fontSize: 20,
+    marginTop: 10,
+    fontWeight: 'bold',
+    fontStyle:'italic',
+    
+  }
+
 });
